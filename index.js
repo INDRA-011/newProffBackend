afa6ad31f84f266a22f8499eec6e86116145809a
INDRA-011 <indr.yznn.77@gmail.com>
2026-02-16 13:56:46 +0545

    database Url added and logic is built

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const connectToDB = require("./db/db");
 const app = express();
 const PORT = 5000;
+
+connectToDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
