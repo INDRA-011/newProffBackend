8dbb00ae9387f8801569602de9967c3f065888cc
INDRA-011 <indr.yznn.77@gmail.com>
2026-02-27 23:42:26 +0545

    new update: password hashing, Jasonwebtoken/jwt integrating and userModel fetch to the database successfully

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const connectToDB = require("./db/db");
 const app = express();
-const PORT = 5000;
+
+app.use(express.json());
+
+connectToDB();
+
+app.use("/auth", require("./routes/userRoute"));
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
@@ -9,4 +20,3 @@ app.listen(PORT, () => {
 app.get("/", (req, res) => {
   res.send("Indra Tamang");
 });
-
